2fa9625ad7fb6b489267faf158086f8d944d397c
moisse <moisesapaza07@gmail.com>
2016-04-16 01:50:40 -0300

    marker in mapbox

--- app/src/main/java/com/technorides/practicemapbox/MainActivity.java
+++ app/src/main/java/com/technorides/practicemapbox/MainActivity.java
@@ -9,20 +9,63 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
 public class MainActivity extends AppCompatActivity {
 
-    //MapView mapView;
-    //private MapView mapView;
+    private double latitudeDefault = -34.603633;
+    private double longitudeDefault = -58.380809;
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mapView = (MapView)findViewById(R.id.mapBoxTechno);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                /*mapboxMap.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng point) {
+
+                    }
+                });*/
+                // Set map style
+                mapboxMap.setStyleUrl(Style.MAPBOX_STREETS);
+
+                // Set the camera's starting position
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(latitudeDefault, longitudeDefault)) // set the camera's center position
+                        .zoom(12)  // set the camera's zoom level
+                        .tilt(20)  // set the camera's tilt
+                        .build();
+
+                // Move the camera to that position
+                mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(latitudeDefault, longitudeDefault))
+                        .title("Hello World!")
+                        .snippet("Welcome Technorides"));
+            }
+        });
+
+        setupFloating();
+    }
+
+    private void setupFloating(){
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,16 +85,41 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
 }
